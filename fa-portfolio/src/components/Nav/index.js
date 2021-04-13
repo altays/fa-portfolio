86370d62400cd533a4fb69d0a8de496627f0593b
Alex Taylor <altays633@gmail.com>
2021-04-13 14:07:31 -0400

    adding bulma classes / syntax to navbar

--- fa-portfolio/src/components/Nav/index.js
+++ fa-portfolio/src/components/Nav/index.js
@@ -3,26 +3,56 @@ import { Link } from "react-router-dom";
 function Nav() {
 
     return (
-    <nav>
-        <ul>
-            <li>
-                <Link to="/">About</Link>
-            </li>
-            <li>
-                <Link to="/music">Music</Link>
-            </li>
-            <li>
-                <Link to="/podcast">Podcast</Link>
-            </li>
-            <li>
-                <Link to="/video">Video</Link>
-            </li>
-            <li>
-                <Link to="/store">Store</Link>
-            </li>
- 
-        </ul>
+    <nav className="navbar" role="navigation" aria-label="main-navigation">
+    
+        <div className="navbar-brand">
+            <a className="navbar-item" href="/">
+                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt=""/>
+            </a>
+
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" href="#dropdown">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+    
+        <div id="navbarBasicExample" className="navbar-menu">
+            {/* need to make the text bigger, spread the entries out more */}
+            <div className="navbar-end">
+                <a className="navbar-item" href="/">
+                    <Link to="/">About</Link>
+                </a>
+
+                <a className="navbar-item" href="/music">
+                    <Link to="/music">Music</Link>
+                </a>
+
+                <a className="navbar-item" href="/podcast">
+                    <Link to="/podcast">Podcast</Link>
+                </a>
+
+                <a className="navbar-item" href="/video">
+                    <Link to="/video">Video</Link>
+                </a>
+
+                <a className="navbar-item" href="store">
+                    <Link to="/store">Store</Link>
+                </a>
+
+                <a className="navbar-item" href="mailto:frenchalexander@gmail.com">
+                    <span className="email-icon">
+                        <i className="fas fa-envelope fa-lg"></i>
+                    </span>
+                </a>
+
+            </div>
+
+        </div>
+    
     </nav>
+
+
     )
 }
 
