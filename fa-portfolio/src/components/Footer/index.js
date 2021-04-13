c1859196e45500dd55fba0f79634fc1341b7a96c
Alex Taylor <altays633@gmail.com>
2021-04-13 14:37:14 -0400

    adding footer component with social icons

---
+++ fa-portfolio/src/components/Footer/index.js
@@ -0,0 +1,43 @@
+function Footer() {
+
+    return (
+
+        <footer class="footer">
+            <div class="content has-text-centered">
+                <a href="mailto:frenchalexander@gmail.com">
+                    <span>
+                        <i className="fas fa-envelope fa-lg"></i>
+                    </span>
+                </a>
+                <a href="https://www.youtube.com/channel/UCKMdpQ2L60I-6koan9PV74w">
+                    <span >
+                        <i className="fab fa-youtube fa-lg"></i>
+                    </span>
+                </a>
+                <a href= "https://twitter.com/frenchatmusic">
+                    <span>
+                        <i className="fab fa-twitter fa-lg"></i>
+                    </span>
+                </a>
+                <a href= "https://www.instagram.com/frenchalexandersomething/">
+                    <span>
+                        <i class="fab fa-instagram fa-lg"></i>
+                    </span>
+                </a>
+                <a href= "https://www.twitch.tv/frenchalexander/">
+                    <span>
+                        <i class="fab fa-twitch fa-lg"></i>
+                    </span>
+                </a>
+            </div>
+            <div class="content has-text-centered">
+                <p>
+                    <a href="#top-of-page">Return to top</a>
+                </p>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer;
