b872e995cbe53ee91827125659454e75195717a2
Alex Taylor <altays633@gmail.com>
2021-05-26 18:51:16 -0400

    replacing photos and background, adding ipsum text

--- fa-portfolio/src/components/pages/About.js
+++ fa-portfolio/src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import pic1 from './images/alex.jpg';
 import pic2 from './images/alex@2x.jpg';
 import pic3 from './images/alex@3x.jpg';
-// import pic4 from 'images/alex@4x.jpg';
+import pic4 from './images/alex@4x.jpg';
 
 function About() {
     return (
@@ -13,7 +13,7 @@ function About() {
                     <div className="picture-shadow"></div>
                     <img id="pictureImage" className="picture-image"
                     src={pic1}
-                    srcSet={`${pic1} 1x,${pic2} 2x,${pic3} 3x`}
+                    srcSet={`${pic1} 1x,${pic2} 2x,${pic3} 3x,${pic4} 4x`}
                     alt="Portrait of Alex Taylor"
                     width="320"
                     height="320" />
@@ -21,15 +21,13 @@ function About() {
             </aside>
 
             <div className="about">
-                <h1 className="name">
+                {/* <h1 className="name">
                     Hi, I'm Alex Taylor
-                </h1>
+                </h1> */}
 
                 <div className="description">
                     <p>
-                        I spend my time traveling the world,
-                        helping startups and tech businesses
-                        hire the best people.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisl sem, tempus non dolor ut, egestas venenatis velit. Integer pellentesque mauris maximus, dapibus sem ut, eleifend mauris. Aliquam suscipit ipsum sed augue vulputate, quis sollicitudin enim placerat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur nibh tortor, faucibus a enim fermentum, tempor porttitor leo. Donec id ipsum efficitur, feugiat augue id, tristique quam. Aenean pretium urna sit amet quam scelerisque commodo. Maecenas tincidunt ex nisl, in convallis felis fringilla vitae. 
                     </p>
                 </div>
 
