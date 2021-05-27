a629c0c5d6c056ff6f25c4b6c3509d652f881dbb
Alex Taylor <altays633@gmail.com>
2021-05-27 15:06:57 -0400

    adding embeds and styling for video page

--- fa-portfolio/src/components/pages/Video.js
+++ fa-portfolio/src/components/pages/Video.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 function Video() {
     return( 
-        <div className="">
+        <section className="video-container">
+            <p className = "video-text">
+                French Alexander creates audiovisual ambient pieces
 
-        </div>
+            </p>
+
+            <div className="video-embed">
+                <a href="#" className="video-link"><iframe width="280" height="280" src="https://www.youtube.com/embed/cvM9JekNgNw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe></a>
+                <a href="#" className="video-link"><iframe width="280" height="280" src="https://www.youtube.com/embed/nvAzH0ac2_4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe></a>
+                <a href="#" className="video-link"><iframe width="280" height="280" src="https://www.youtube.com/embed/aft0OaOdGjA?start=1469" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe></a>
+            </div>
+
+            <p className = "video-text">
+                Check out videos like this and more on <a href="https://www.youtube.com/channel/UCKMdpQ2L60I-6koan9PV74w/videos">YouTube</a>.
+            </p>
+
+            
+
+        </section>
 
         
     );
