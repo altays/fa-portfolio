8b918ad018a56d38b2c0799b7f87088b341656e1
Alex Taylor <altays633@gmail.com>
2021-05-27 12:02:08 -0400

    adding data, rendering, and styling for albums

--- fa-portfolio/src/components/pages/Sound.js
+++ fa-portfolio/src/components/pages/Sound.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { musicData } from "./music";
+import ReactHtmlParser from 'react-html-parser';
 
 function Sound() {
     return (
-    <section>
-
-    </section>
-    )
-;
+        <>
+        <div className="album-container">
+          {musicData.map((data, key) => {
+            return (
+              <div key={key}>
+                <Album
+                  key={key}
+                  AlbumName={data.AlbumName}
+                  Embed={data.Embed}
+                  URL = {data.URL}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
 }
 
 export default Sound;
+
+const Album = ({ AlbumName, Embed, URL }) => {
+
+    if (!AlbumName) return <div>"nothing here lol"</div> ;
+    return (
+        <div className="album">
+            <a href={URL}>{ReactHtmlParser(Embed)}</a>
+        </div>
+    )
+};
+
